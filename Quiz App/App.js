1d785dd4e890a99f600ef0a7d3bbecad4a1bc33d
MuznaWajid <57355914+MuznaWajid@users.noreply.github.com>
2020-07-27 22:44:52 +0500

    Updated files

---
+++ Quiz App/App.js
@@ -0,0 +1,87 @@
+var questions=[{
+    question: "Who invented JavaScript?",
+    answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
+},
+correctAnswer: "c"},
+{
+    question: "Which one of these is a JavaScript package manager?",
+    answers: {
+      a: "Node.js",
+      b: "TypeScript",
+      c: "npm"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    answers: {
+      a: "Angular",
+      b: "jQuery",
+      c: "RequireJS",
+      d: "ESLint"
+    },
+    correctAnswer: "d"
+  },
+  {
+    question: " Which of the following is a server-side Java Script object?",
+    answers: {
+      a: "File",
+      b: "Fileupload",
+      c: "Function",
+      d: "Data"
+    },
+    correctAnswer: "a"
+  },
+
+  {
+    question: " Javascript can be used for storing form's contents to a database file on the server",
+    answers: {
+      a: "False",
+      b: "True",
+      
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: " To insert a JavaScript into an HTML page, which tag is used?",
+    answers: {
+      a: "<javascript>",
+      b: "<js>",
+      c: "<script>",
+      d: "<script = java>"
+      
+    },
+    correctAnswer: "c"
+  },{
+    question: "Which of the below is used in Java script to insert special characters?",
+    answers: {
+      a: "-",
+      b: " \ ",
+      c: "&",
+      d: "%"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "JavaScript ignores extra spaces?",
+    answers: {
+      a: "False",
+      b: " True ",
+      c: "Non of these"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "Which of the following is used to capture all click events in a window?",
+    answers: {
+      a: "window.routeEvents(Event.CLICK );",
+      b: " window.raiseEvents(Event.CLICK );",
+      c: "window.handleEvents (Event.CLICK);",
+      d: "window.captureEvents(Event.CLICK);"
+    },
+    correctAnswer: "c"
+  },
+];
